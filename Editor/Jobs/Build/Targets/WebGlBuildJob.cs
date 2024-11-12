648e468fbbdd7ece88ecd5829ceb2f8cc3c6ef13
Anton Alexeyev <anton@gameonproduction.com>
2024-11-12 15:00:20 +0700

    fix: bug in SemVer, add WebGL build job

---
+++ Editor/Jobs/Build/Targets/WebGlBuildJob.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace CiWizard.Editor.Jobs.Build.Targets {
+    [CreateAssetMenu(fileName = "WebGL", menuName = "CI/Jobs/Build/WebGL")]
+    public class WebGlBuildJob : AbstractBuildJob {
+        protected override BuildTarget JobBuildTarget => BuildTarget.WebGL;
+        protected override string FileExtension => "";
+    }
+}
